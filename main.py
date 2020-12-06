354798245bd19ddd8404b35b72cf1e7fcc96b5dc
arazi_chen <arazic@post.bgu.ac.il>
2020-12-06 09:59:03 +0200

    interview by company name

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ import urllib
 import datetime as dt
 
 start = time.time()
+interview_jobs= ['Software']
 
 DEFAULT_URL = ('https://www.glassdoor.com/Interview/index.htm')
 
@@ -112,239 +113,60 @@ logging.getLogger('selenium').setLevel(logging.CRITICAL)
 logging.getLogger('selenium').setLevel(logging.CRITICAL)
 
 
-def scrape(field, review, author):
+def scrape(field, interview):
 
-    def scrape_date(review):
-        return review.find_element_by_tag_name(
-            'time').get_attribute('datetime')
+    def scrape_emp_title(interview):
+        return interview[1]
 
-    def scrape_emp_title(review):
-        if 'Anonymous Employee' not in review.text:
-            try:
-                res = author.find_element_by_class_name(
-                    'authorJobTitle').text.split('-')[1]
-            except Exception:
-                logger.warning('Failed to scrape employee_title')
-                res = np.nan
-        else:
-            res = np.nan
-        return res
+    def scrape_application(interview):
+        flag= False
+        app=""
+        for line in interview:
+            if line=='Application':
+                flag= True
+            elif line!='Application' and not flag:
+                continue
+            elif line!='Application' and flag:
+                app=app+ line+ " "
+        return app
 
-    def scrape_location(review):
-        if 'in' in review.text:
-            try:
-                res = author.find_element_by_class_name(
-                    'authorLocation').text
-            except Exception:
-                res = np.nan
-        else:
-            res = np.nan
-        return res
+    def scrape_interview(interview):
+        flag = False
+        Interview = ""
+        for line in interview:
+            if line == 'Interview':
+                flag = True
+            elif line != 'Interview' and not flag:
+                continue
+            elif line != 'Interview' and flag:
+                app = app + line + " "
+        return app
 
-    def scrape_status(review):
-        try:
-            res = author.text.split('-')[0]
-        except Exception:
-            logger.warning('Failed to scrape employee_status')
-            res = np.nan
-        return res
-
-    def scrape_rev_title(review):
-        return review.find_element_by_class_name('summary').text.strip('"')
-
-    def scrape_years(review):
-        print('review',  review.text)
-        res = review.find_element_by_class_name('common__EiReviewTextStyles__allowLineBreaks').find_element_by_xpath('preceding-sibling::p').text
-        return res
-
-    def scrape_helpful(review):
-        try:
-            helpful = review.find_element_by_class_name('helpfulCount')
-            res = helpful[helpful.find('(') + 1: -1]
-        except Exception:
-            res = 0
-        return res
-
-    def expand_show_more(section):
-        try:
-            # more_content = section.find_element_by_class_name('moreContent')
-            more_link = section.find_element_by_class_name('v2__EIReviewDetailsV2__continueReading')
-            more_link.click()
-        except Exception:
-            pass
-
-    def scrape_pros(review):
-        try:
-            pros = review.find_element_by_class_name('common__EiReviewTextStyles__allowLineBreaks')
-            expand_show_more(pros)
-            res = pros.text.replace('Pros', '')
-            res = res.strip()
-        except Exception:
-            res = np.nan
-        return res
-
-    def scrape_cons(review):
-        try:
-            cons = review.find_elements_by_class_name('common__EiReviewTextStyles__allowLineBreaks')[1]
-            expand_show_more(cons)
-            res = cons.text.replace('Cons', '')
-            res = res.strip()
-        except Exception:
-            res = np.nan
-        return res
-
-    def scrape_advice(review):
-        try:
-            advice = review.find_elements_by_class_name('common__EiReviewTextStyles__allowLineBreaks')[2]
-            res = advice.text.replace('Advice to Management', '')
-            res = res.strip()
-        except Exception:
-            res = np.nan
-        return res
-
-    def scrape_overall_rating(review):
-        try:
-            ratings = review.find_element_by_class_name('gdStars')
-            overall = ratings.find_element_by_class_name(
-                'rating').find_element_by_class_name('value-title')
-            res = overall.get_attribute('title')
-        except Exception:
-            res = np.nan
-        return res
-
-    def _scrape_subrating(i):
-        try:
-            ratings = review.find_element_by_class_name('gdStars')
-            subratings = ratings.find_element_by_class_name(
-                'subRatings').find_element_by_tag_name('ul')
-            this_one = subratings.find_elements_by_tag_name('li')[i]
-            res = this_one.find_element_by_class_name(
-                'gdBars').get_attribute('title')
-        except Exception:
-            res = np.nan
-        return res
-
-    def scrape_work_life_balance(review):
-        return _scrape_subrating(0)
-
-    def scrape_culture_and_values(review):
-        return _scrape_subrating(1)
-
-    def scrape_career_opportunities(review):
-        return _scrape_subrating(2)
-
-    def scrape_comp_and_benefits(review):
-        return _scrape_subrating(3)
-
-    def scrape_senior_management(review):
-        return _scrape_subrating(4)
-
-
-    def scrape_recommends(review):
-        try:
-            res = review.find_element_by_class_name('recommends').text
-            res = res.split('\n')
-            return res[0]
-        except:
-            return np.nan
-    
-    def scrape_outlook(review):
-        try:
-            res = review.find_element_by_class_name('recommends').text
-            res = res.split('\n')
-            if len(res) == 2 or len(res) == 3:
-                if 'CEO' in res[1]:
-                    return np.nan
-                return res[1]
-            return np.nan
-        except:
-            return np.nan
-    
-    def scrape_approve_ceo(review):
-        try:
-            res = review.find_element_by_class_name('recommends').text
-            res = res.split('\n')
-            if len(res) == 3:
-                return res[2]
-            if len(res) == 2:
-                if 'CEO' in res[1]:
-                    return res[1]
-            return np.nan
-        except:
-            return np.nan
+    def scrape_interview_questions(interview):
+        flag = False
+        answer_questions = ""
+        for line in interview:
+            if line == 'Answer Questions':
+                flag = True
+            elif line != 'Answer Questions' and not flag:
+                continue
+            elif line != 'Answer Questions' and flag:
+                app = app + line + " "
+        return app
 
 
     funcs = [
-        scrape_date,
         scrape_emp_title,
-        scrape_location,
-        scrape_status,
-        scrape_rev_title,
-        scrape_years,
-        scrape_helpful,
-        scrape_pros,
-        scrape_cons,
-        scrape_advice,
-        scrape_overall_rating,
-        scrape_work_life_balance,
-        scrape_culture_and_values,
-        scrape_career_opportunities,
-        scrape_comp_and_benefits,
-        scrape_senior_management,
-        scrape_recommends,
-        scrape_outlook,
-        scrape_approve_ceo
-
+        scrape_application,
+        scrape_interview,
+        scrape_interview_questions
     ]
 
     fdict = dict((s, f) for (s, f) in zip(SCHEMA, funcs))
+    fdict['Job title'](interview)
+    fdict['Application'](interview)
 
-    return fdict[field](review)
-
-
-def extract_from_page():
-
-    def is_featured(review):
-        try:
-            review.find_element_by_class_name('featuredFlag')
-            return True
-        except selenium.common.exceptions.NoSuchElementException:
-            return False
-
-    def extract_review(review):
-        author = review.find_element_by_class_name('authorInfo')
-        res = {}
-        # import pdb;pdb.set_trace()
-        for field in SCHEMA:
-            res[field] = scrape(field, review, author)
-
-        assert set(res.keys()) == set(SCHEMA)
-        return res
-
-    logger.info(f'Extracting reviews from page {page[0]}')
-
-    res = pd.DataFrame([], columns=SCHEMA)
-
-    reviews = browser.find_elements_by_class_name('empReview')
-    logger.info(f'Found {len(reviews)} reviews on page {page[0]}')
-
-    for review in reviews:
-        if not is_featured(review):
-            data = extract_review(review)
-            logger.info(f'Scraped data for "{data["review_title"]}"\({data["date"]})')
-            res.loc[idx[0]] = data
-        else:
-            logger.info('Discarding a featured review')
-        idx[0] = idx[0] + 1
-
-    if args.max_date and \
-        (pd.to_datetime(res['date']).max() > args.max_date) or \
-            args.min_date and \
-            (pd.to_datetime(res['date']).min() < args.min_date):
-        logger.info('Date limit reached, ending process')
-        date_limit_reached[0] = True
-
-    return res
+    return fdict[field](interview)
 
 
 def more_pages():
@@ -376,27 +198,6 @@ def no_interviews():
     return False
     # TODO: Find a company with no interviews to test on
 
-
-def navigate_to_reviews():
-    logger.info('Navigating to company reviews')
-
-    browser.get(args.url)
-    time.sleep(1)
-
-    if no_reviews():
-        logger.info('No reviews to scrape. Bailing!')
-        return False
-
-    reviews_cell = browser.find_element_by_xpath(
-        '//a[@data-label="Reviews"]')
-    reviews_path = reviews_cell.get_attribute('href')
-    
-    # reviews_path = driver.current_url.replace('Overview','Reviews')
-    browser.get(reviews_path)
-    time.sleep(1)
-    return True
-
-
 def navigate_to_interviews():
     logger.info('Navigating to company interviews')
 
@@ -407,27 +208,26 @@ def navigate_to_interviews():
     inputElement.send_keys('nice')
     inputElement.send_keys(Keys.ENTER)
 
-    print('here')
 
-    interviews_place = browser.find_elements_by_class_name("interviewEmployerList")[0]
-    interviews_place_path = interviews_place.find_element_by_tag_name('a').get_attribute('href')
+    try:
+        interviews_place = browser.find_elements_by_class_name("interviewEmployerList")[0]
+        time.sleep(1)
+        time.sleep(1)
+    except:
+        pass
 
-    browser.get(interviews_place_path)
     time.sleep(1)
-    return True
+    interviews_place_path = interviews_place.find_element_by_tag_name('a').get_attribute('href')
+    browser.get(interviews_place_path)
+    interviews_details = browser.find_element_by_tag_name('ol').text.split('Helpful')
+
+    time.sleep(1)
+    return interviews_details
 
 
     if no_interviews():
         logger.info('No interviews to scrape. Bailing!')
         return False
-
-    # interviews_cell = browser.find_element_by_xpath(
-    #     '//a[@data-label="Inter­views"]')
-    # interviews_path = interviews_cell.get_attribute('href')
-    #
-    # browser.get(interviews_path)
-    # time.sleep(1)
-    # return True
 
 
 def sign_in():
@@ -448,8 +248,6 @@ def sign_in():
 
     time.sleep(3)
     browser.get(args.url)
-
-
 
 def get_browser():
     logger.info('Configuring browser')
@@ -491,79 +289,36 @@ page = [1]
 idx = [0]
 date_limit_reached = [False]
 
+def extract_interviews_from_page(interviews):
 
-def main():
-
-    logger.info(f'Scraping up to {args.limit} reviews.')
-
-    res = pd.DataFrame([], columns=SCHEMA)
-
-    sign_in()
-
-    if not args.start_from_url:
-        reviews_exist = navigate_to_reviews()
-        if not reviews_exist:
-            return
-    elif args.max_date or args.min_date:
-        verify_date_sorting()
-        browser.get(args.url)
-        page[0] = get_current_page()
-        logger.info(f'Starting from page {page[0]:,}.')
-        time.sleep(1)
-    else:
-        browser.get(args.url)
-        page[0] = get_current_page()
-        logger.info(f'Starting from page {page[0]:,}.')
-        time.sleep(1)
-
-    reviews_df = extract_from_page()
-    res = res.append(reviews_df)
-
-    # import pdb;pdb.set_trace()
-
-    while more_pages() and\
-            len(res) < args.limit and\
-            not date_limit_reached[0]:
-        go_to_next_page()
-        reviews_df = extract_from_page()
-        res = res.append(reviews_df)
-
-    logger.info(f'Writing {len(res)} reviews to file {args.file}')
-    res.to_csv(args.file, index=False, encoding='utf-8')
-
-    end = time.time()
-    logger.info(f'Finished in {end - start} seconds')
-
-
-def extract_interviews_from_page():
-
-    def is_featured(interview):
+    def is_relevant_interview(interview, interview_jobs):
         try:
-            interview.find_element_by_class_name('featuredFlag')
-            return True
+            interview_details= interview.split('\n')
+            if len(interview_details)>1:
+                interview_job= interview_details[1]
+
+                interview_job_words= interview_job.split(' ')
+                for word in interview_job_words:
+                    if(word in interview_jobs):
+                        return True
+                else:
+                    return False
         except selenium.common.exceptions.NoSuchElementException:
             return False
 
-    def extract_review(review):
-        author = review.find_element_by_class_name('authorInfo')
-        res = {}
-        # import pdb;pdb.set_trace()
+    def extract_interview(interview):
+        interview= interview.split('\n')
         for field in SCHEMA:
-            res[field] = scrape(field, review, author)
+            res[field] = scrape(field, interview)
 
         assert set(res.keys()) == set(SCHEMA)
         return res
 
-    logger.info(f'Extracting reviews from page {page[0]}')
+    res = {}
 
-    res = pd.DataFrame([], columns=SCHEMA)
-
-    reviews = browser.find_elements_by_class_name('empReview')
-    logger.info(f'Found {len(reviews)} reviews on page {page[0]}')
-
-    for review in reviews:
-        if not is_featured(review):
-            data = extract_review(review)
+    for interview in interviews:
+        if is_relevant_interview(interview, interview_jobs):
+            data = extract_interview(interview)
             logger.info(f'Scraped data for "{data["review_title"]}"\({data["date"]})')
             res.loc[idx[0]] = data
         else:
@@ -586,7 +341,7 @@ def interview_main():
     res = pd.DataFrame([], columns=SCHEMA)
 
     sign_in()
-
+    interviews_exist=[]
     if not args.start_from_url:
         interviews_exist = navigate_to_interviews()
         if not interviews_exist:
@@ -597,17 +352,16 @@ def interview_main():
         logger.info(f'Starting from page {page[0]:,}.')
         time.sleep(1)
 
-    interviews_df = extract_interviews_from_page()
+    interviews_df = extract_interviews_from_page(interviews_exist)
     res = res.append(interviews_df)
 
-    # import pdb;pdb.set_trace()
 
     while more_pages() and \
             len(res) < args.limit and \
             not date_limit_reached[0]:
         go_to_next_page()
-        reviews_df = extract_from_page()
-        res = res.append(reviews_df)
+        interviews_exist = navigate_to_interviews()
+        res = res.append(interviews_exist)
 
     logger.info(f'Writing {len(res)} reviews to file {args.file}')
     res.to_csv(args.file, index=False, encoding='utf-8')
